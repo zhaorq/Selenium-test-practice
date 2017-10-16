f61d1361ff494008a9b647d9865f903deddbf17d
zhaorq <zhaorq@gmail.com>
2017-10-15 20:08:25 -0700

    [update] Conduct one test, working on how to run several tests with different data

--- numCompareTest.js
+++ numCompareTest.js
@@ -5,14 +5,20 @@ var browser = new webdriver.Builder().forBrowser('chrome').build();
 var browsers = ['Firefox','Chrome','Safari'];
  
 
-function testNumber() {
-
+function compareNumber(num1,num2,compareNum) {
+  let result; 
+  if(compareNum>=num1 && compareNum<=num2)
+    result = `${compareNum} is between ${num1} and ${num2}` 
+  else 
+    result = `${compareNum} is not between ${num1} and ${num2}` 
+  
+  return result; 
 }
 
 
 var randomNumber1 = Math.floor(Math.random()*200-100).toString(),
 randomNumber2 = Math.floor(Math.random()*200-100).toString(),
-randomNumber3 = Math.floor(Math.random()*200-100).toString(); 
+randomNumberCompare = Math.floor(Math.random()*200-100).toString(); 
 
 function checkTitle() {
     browser.getTitle()
@@ -34,13 +40,17 @@ function closeBrowser() {
 browser.get('http://localhost:3000/');
 browser.findElement(webdriver.By.name('numFirst')).sendKeys(randomNumber1);
 browser.findElement(webdriver.By.name('numSecond')).sendKeys(randomNumber2);
-browser.findElement(webdriver.By.name('numToCompare')).sendKeys(randomNumber3); 
+browser.findElement(webdriver.By.name('numToCompare')).sendKeys(randomNumberCompare); 
 browser.findElement(webdriver.By.xpath('//*[@id="app"]/div/div/form/input')).click()
 .then(
     browser.findElement(webdriver.By.xpath('//*[@id="app"]/div/div/div[2]/div/div[2]/p'))
 .getText()
 .then((result) => {
-    console.log('result ',result); 
+  let expectResult = compareNumber(randomNumber1,randomNumber2,randomNumberCompare); 
+  if(result === expectResult) 
+    console.log('Test passed, result is correct.')
+  else 
+    console.log('Test failed, result is incorrect.')
 })
 )
 
