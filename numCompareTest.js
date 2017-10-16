50f24fc440210af29baba9c46f7428727ec3a5c0
zhaorq <zhaorq@gmail.com>
2017-10-15 20:41:26 -0700

    [update] Performed several tests with different testing data

--- numCompareTest.js
+++ numCompareTest.js
@@ -1,56 +1,66 @@
 
 var webdriver = require('selenium-webdriver');
-var browser = new webdriver.Builder().forBrowser('chrome').build();
 
-var browsers = ['Firefox','Chrome','Safari'];
- 
+// var browsers = ['Firefox','Chrome','Safari'];
 
-function compareNumber(num1,num2,compareNum) {
-  let result; 
-  if(compareNum>=num1 && compareNum<=num2)
-    result = `${compareNum} is between ${num1} and ${num2}` 
-  else 
-    result = `${compareNum} is not between ${num1} and ${num2}` 
-  
-  return result; 
+function compareNumber(num1, num2, compareNum) {
+  let result;
+  if ((num1 - compareNum) * (num2 - compareNum) <= 0)
+    result = `${compareNum} is between ${num1} and ${num2}`
+  else
+    result = `${compareNum} is not between ${num1} and ${num2}`
+
+  return result;
+}
+
+var testData = (times) => {
+  let testData = [];
+  while (times > 0) {
+    let randomNumber1 = Math.floor(Math.random() * 200 - 100).toString(),
+      randomNumber2 = Math.floor(Math.random() * 200 - 100).toString(),
+      randomNumberCompare = Math.floor(Math.random() * 200 - 100).toString();
+    testData.push([randomNumber1, randomNumber2, randomNumberCompare]);
+    times--;
+  }
+  return testData;
 }
 
 
-var randomNumber1 = Math.floor(Math.random()*200-100).toString(),
-randomNumber2 = Math.floor(Math.random()*200-100).toString(),
-randomNumberCompare = Math.floor(Math.random()*200-100).toString(); 
+// function checkTitle() {
+//     browser.getTitle()
+//     .then(function(title) {
+//          console.log("The title is: " + title)
+//      });
+// }
 
-function checkTitle() {
-    browser.getTitle()
-    .then(function(title) {
-         console.log("The title is: " + title)
-     });
-}
-
-function handleFailure(err) {
-	console.error('Something went wrong\n', err.stack, '\n');
-	closeBrowser();
-}
+// function handleFailure(err) {
+// 	console.error('Something went wrong\n', err.stack, '\n');
+// 	closeBrowser();
+// }
 
 
-function closeBrowser() {
-	browser.quit();
-}
-
-browser.get('http://localhost:3000/');
-browser.findElement(webdriver.By.name('numFirst')).sendKeys(randomNumber1);
-browser.findElement(webdriver.By.name('numSecond')).sendKeys(randomNumber2);
-browser.findElement(webdriver.By.name('numToCompare')).sendKeys(randomNumberCompare); 
-browser.findElement(webdriver.By.xpath('//*[@id="app"]/div/div/form/input')).click()
-.then(
+testData(5).map((data) => {
+  var browser = new webdriver.Builder().forBrowser('chrome').build();
+  browser.get('http://localhost:3000/')
+  let firstNum = data[0], secondNum = data[1], compareNum = data[2];
+  browser.findElement(webdriver.By.name('numFirst')).sendKeys(firstNum);
+  browser.findElement(webdriver.By.name('numSecond')).sendKeys(secondNum);
+  browser.findElement(webdriver.By.name('numToCompare')).sendKeys(compareNum);
+  browser.findElement(webdriver.By.xpath('//*[@id="app"]/div/div/form/input')).click()
+    .then(
     browser.findElement(webdriver.By.xpath('//*[@id="app"]/div/div/div[2]/div/div[2]/p'))
-.getText()
-.then((result) => {
-  let expectResult = compareNumber(randomNumber1,randomNumber2,randomNumberCompare); 
-  if(result === expectResult) 
-    console.log('Test passed, result is correct.')
-  else 
-    console.log('Test failed, result is incorrect.')
+      .getText()
+      .then((result) => {
+        let expectResult = compareNumber(firstNum, secondNum, compareNum);
+        if (result === expectResult)
+          console.log('Test passed, result is correct.')
+        else
+          console.log(`Test failed, result is incorrect. Expecting ${expectResult} but got ${result}`)
+      })
+    )
+  browser.quit();
 })
-)
 
+
+
+// .then(closeBrowser, handleFailure);
